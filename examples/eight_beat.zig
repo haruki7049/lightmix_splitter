c349397dc211c93f5024dec2aa5ed616d9a938eb
haruki7049 <tontonkirikiri@gmail.com>
2026-03-27 20:11:29 +0900

    Update eight_beat.zig

--- examples/eight_beat.zig
+++ examples/eight_beat.zig
@@ -8,46 +8,113 @@ const sample_rate = 44100;
 const channels = 1;
 
 pub fn gen(allocator: std.mem.Allocator) anyerror!lightmix.Wave(SamplingType) {
+    // Select BPM
+    const bpm = 170;
+
     // A number of samples per beat
-    const spb = samples_per_beat(120, sample_rate);
+    const spb = samples_per_beat(bpm, sample_rate);
 
-    var waves: [16]?lightmix.Wave(SamplingType) = undefined;
-    for (waves, 0..) |_, i| {
-        const is_timing = i % 4 == 0 or i % 4 == 2 or i % 4 == 3;
+    // Composer
+    var composer = lightmix.Composer(SamplingType).init(allocator, .{
+        .sample_rate = sample_rate,
+        .channels = channels,
+    });
+    defer {
+        for (composer.info) |waveinfo| {
+            waveinfo.wave.deinit();
+        }
+        composer.deinit();
+    }
 
-        if (is_timing) {
+    // Kick drums
+    {
+        var kickdrums: [16]?lightmix.Wave(SamplingType) = undefined;
+        for (kickdrums, 0..) |_, i| {
             var w: lightmix.Wave(SamplingType) = try lightmix_synths.Basic.Sine.gen(SamplingType, .{
                 .allocator = allocator,
                 .amplitude = 1.0,
-                .frequency = 220.0,
+                .frequency = 110.0,
                 .length = spb,
                 .sample_rate = sample_rate,
                 .channels = channels,
             });
+            try w.filter_with(lightmix_filters.volume.CutAttackArgs, lightmix_filters.volume.cutAttack, .{ .start_point = 0, .length = 600 });
+            try w.filter_with(lightmix_filters.volume.DecayArgs, lightmix_filters.volume.decay, .{});
+            try w.filter_with(lightmix_filters.volume.DecayArgs, lightmix_filters.volume.decay, .{});
             try w.filter_with(lightmix_filters.volume.DecayArgs, lightmix_filters.volume.decay, .{});
             try w.filter_with(lightmix_filters.volume.DecayArgs, lightmix_filters.volume.decay, .{});
             try w.filter_with(lightmix_filters.volume.DecayArgs, lightmix_filters.volume.decay, .{});
             try w.filter_with(lightmix_filters.volume.DecayArgs, lightmix_filters.volume.decay, .{});
 
-            waves[i] = w;
-        } else {
-            waves[i] = null;
+            kickdrums[i] = w;
         }
+        defer for (kickdrums) |wave| {
+            if (wave != null) {
+                wave.?.deinit();
+            }
+        };
+
+        const result: lightmix.Wave(f64) = try splitter_gen(SamplingType, .{
+            .allocator = allocator,
+            .amplitude = 1.0,
+            .length = spb * 16,
+            .waves = &kickdrums,
+            .sample_rate = sample_rate,
+            .channels = channels,
+        });
+        try composer.append(.{ .start_point = 0, .wave = result });
     }
-    defer for (waves) |wave| {
-        if (wave != null) {
-            wave.?.deinit();
-        }
-    };
 
-    const result: lightmix.Wave(f64) = try splitter_gen(SamplingType, .{
-        .allocator = allocator,
-        .amplitude = 1.0,
-        .length = spb * 8,
-        .waves = &waves,
-        .sample_rate = sample_rate,
-        .channels = channels,
-    });
+    // Symbal
+    {
+        var symbals: [16]?lightmix.Wave(SamplingType) = undefined;
+        for (symbals, 0..) |_, i| {
+            const is_timing = i % 4 == 2;
+            std.debug.print("{any}\n", .{is_timing});
+
+            if (is_timing) {
+                var w: lightmix.Wave(SamplingType) = try lightmix_synths.Basic.Triangle.gen(SamplingType, .{
+                    .allocator = allocator,
+                    .amplitude = 1.0,
+                    .frequency = 110.0,
+                    .length = spb,
+                    .sample_rate = sample_rate,
+                    .channels = channels,
+                });
+                try w.filter_with(lightmix_filters.volume.CutAttackArgs, lightmix_filters.volume.cutAttack, .{ .start_point = 0, .length = 600 });
+                try w.filter_with(lightmix_filters.volume.DecayArgs, lightmix_filters.volume.decay, .{});
+                try w.filter_with(lightmix_filters.volume.DecayArgs, lightmix_filters.volume.decay, .{});
+                try w.filter_with(lightmix_filters.volume.DecayArgs, lightmix_filters.volume.decay, .{});
+
+                symbals[i] = w;
+            } else {
+                symbals[i] = null;
+            }
+        }
+
+        std.debug.print("Defer\n", .{});
+        defer for (symbals) |wave| {
+            if (wave != null) {
+                wave.?.deinit();
+            }
+        };
+
+        std.debug.print("Creating result\n", .{});
+        const result: lightmix.Wave(f64) = try splitter_gen(SamplingType, .{
+            .allocator = allocator,
+            .amplitude = 1.0,
+            .length = spb * 16,
+            .waves = &symbals,
+            .sample_rate = sample_rate,
+            .channels = channels,
+        });
+
+        std.debug.print("Appending result to composer\n", .{});
+        try composer.append(.{ .start_point = 0, .wave = result });
+    }
+
+    std.debug.print("Finalize\n", .{});
+    const result = try composer.finalize(.{});
     return result;
 }
 
